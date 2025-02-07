5e9af32805491a5c6ca25736967ae4c205aa6e00
RealHatawa <nandeyanen346@gmail.com>
2025-02-07 13:32:32 +0900

    Add base project structure

---
+++ src/main/java/com/scit/mojimoji/MojimojiApplication.java
@@ -0,0 +1,13 @@
+package com.scit.mojimoji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MojimojiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MojimojiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scit/mojimoji/MojimojiApplicationTests.java
@@ -0,0 +1,12 @@
+package com.scit.mojimoji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MojimojiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
