ad513ded255601a0bc3e55a5c7b49ee3d177f424
mk0131 <jmg21128@gmail.com>
2025-02-13 12:00:53 +0900

    Feat: H2 더미데이터 껍데기 작성

---
+++ src/main/resources/database/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO users 
+VALUES ('2025-02-13 10:56:14.092749', '2025-02-13 10:56:14.092749', null, 'ROLE_USER', 
+        '지익준', 'dlrwns', 1234, 'dlrwns@gmail.com', NULL, 'INACTIVE');
+INSERT INTO users 
+VALUES ('2025-02-13 10:56:14.092749', '2025-02-13 10:56:14.092749', null, 'ROLE_USER', 
+        '장진식', 'wlstlr', 1234, 'wlstlr@gmail.com', NULL, 'INACTIVE');
+INSERT INTO users 
+VALUES ('2025-02-13 10:56:14.092749', '2025-02-13 10:56:14.092749', null, 'ROLE_USER', 
+        '死ね', 'wnrwk', 1234, 'test@gmail.com', NULL, 'INACTIVE');
+        
