e3813627b704403103323029b0e8e6c40bd11337
yewon <ipunwon_@naver.com>
2025-02-18 22:16:36 +0900

    공유된 스토리 목록 검색&정렬 구현

--- src/main/java/com/sangkeumi/mojimoji/repository/SharedBookRepository.java
+++ src/main/java/com/sangkeumi/mojimoji/repository/SharedBookRepository.java
@@ -1,7 +1,9 @@
 package com.sangkeumi.mojimoji.repository;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.sangkeumi.mojimoji.entity.SharedBook;
@@ -15,5 +17,11 @@ public interface SharedBookRepository extends JpaRepository<SharedBook, Long> {
      * @return
      */
     Optional<SharedBook> findByBook_bookId(Long bookId);
+
+    // 키워드가 포함된 제목을 검색하며, 주어진 정렬 옵션으로 결과를 정렬
+    List<SharedBook> findByBook_TitleContainingIgnoreCase(String keyword, Sort sort);
+
+    // 키워드가 포함된 작성자(닉네임)를 검색하며, 주어진 정렬 옵션으로 결과를 정렬
+    List<SharedBook> findByBook_User_NicknameContainingIgnoreCase(String keyword, Sort sort);
 
 }
