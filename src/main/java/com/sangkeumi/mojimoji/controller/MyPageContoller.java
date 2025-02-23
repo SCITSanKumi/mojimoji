9f8a88b3331769136afbe4215204770aba634e7a
mk0131 <jmg21128@gmail.com>
2025-02-23 23:21:17 +0900

    차트 연습

--- src/main/java/com/sangkeumi/mojimoji/controller/MyPageContoller.java
+++ src/main/java/com/sangkeumi/mojimoji/controller/MyPageContoller.java
@@ -1,12 +1,20 @@
 package com.sangkeumi.mojimoji.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sangkeumi.mojimoji.dto.user.MyPrincipal;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Controller
+@Slf4j
 public class MyPageContoller {
     @GetMapping("/mypage")
-    public String mypage() {
+    public String mypage(@AuthenticationPrincipal MyPrincipal myPrincipalm, Model model) {
+
         return "/mypage";
     }
 }
