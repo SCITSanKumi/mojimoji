1fb7faff06f343de6ddd7f85110dba0d913ed352
JO MINGYU <jmg21128@gmail.com>
2025-03-11 19:01:41 +0900

    Rank (#36)
    * Docs: 여러가지 구조 변경
    
    * Fix: 오답은 컬렉트 안함
    
    * Design: 여러가지 디자인 수정
    
    * Fix: 헤더 위치 수정
    
    * Fix: 보이는 에러 전부 수정

--- src/main/java/com/sangkeumi/mojimoji/controller/KanjiController.java
+++ src/main/java/com/sangkeumi/mojimoji/controller/KanjiController.java
@@ -30,138 +30,138 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 @Tag(name = "Kanji API", description = "한자 관련 API")
 public class KanjiController {
 
-        private final KanjiCollectionService kanjiCollectionService;
+  private final KanjiCollectionService kanjiCollectionService;
 
-        @GetMapping("/collection")
-        public String kanjiCollection(
-                        @AuthenticationPrincipal MyPrincipal principal,
-                        @ModelAttribute KanjiSearchRequest searchRequest,
-                        Model model) {
-                int page = 1;
+  @GetMapping("/collection")
+  public String kanjiCollection(
+      @AuthenticationPrincipal MyPrincipal principal,
+      @ModelAttribute KanjiSearchRequest searchRequest,
+      Model model) {
+    int page = 1;
 
-                Page<KanjiSearchResponse> searchResponse = kanjiCollectionService.getMyCollection(principal.getUserId(),
-                                searchRequest, page);
+    Page<KanjiSearchResponse> searchResponse = kanjiCollectionService.getMyCollection(principal.getUserId(),
+        searchRequest, page);
 
-                List<WrongKanji> wrongKanji = kanjiCollectionService.getWrongKanji(principal.getUserId());
+    List<WrongKanji> wrongKanji = kanjiCollectionService.getWrongKanji(principal.getUserId());
 
-                KanjiCount countDto = kanjiCollectionService.findTotalAndCollected(searchRequest,
-                                principal.getUserId());
+    KanjiCount countDto = kanjiCollectionService.findTotalAndCollected(searchRequest,
+        principal.getUserId());
 
-                Long totalCount = (countDto.getTotalCount() != null) ? countDto.getTotalCount() : 0;
-                Long collectedCount = (countDto.getCollectedCount() != null) ? countDto.getCollectedCount() : 0;
+    Long totalCount = (countDto.getTotalCount() != null) ? countDto.getTotalCount() : 0;
+    Long collectedCount = (countDto.getCollectedCount() != null) ? countDto.getCollectedCount() : 0;
 
-                model.addAttribute("searchRequest", searchRequest);
-                model.addAttribute("searchResponse", searchResponse.getContent());
-                model.addAttribute("wrongKanji", wrongKanji);
-                // 여기서 "전체 결과" 기준으로 collectedCount / totalCount
-                model.addAttribute("totalCount", totalCount);
-                model.addAttribute("collected", collectedCount);
+    model.addAttribute("searchRequest", searchRequest);
+    model.addAttribute("searchResponse", searchResponse.getContent());
+    model.addAttribute("wrongKanji", wrongKanji);
+    // 여기서 "전체 결과" 기준으로 collectedCount / totalCount
+    model.addAttribute("totalCount", totalCount);
+    model.addAttribute("collected", collectedCount);
 
-                return "kanji/kanjiCollection";
-        }
+    return "kanji/kanjiCollection";
+  }
 
-        /**
-         * 무한 스크롤로 page=2,3... 요청 시 AJAX로 partial HTML 반환
-         */
-        @GetMapping("/collectionAjax")
-        @Operation(summary = "한자 목록 AJAX", description = "추가 10개 로딩")
+  /**
+   * 무한 스크롤로 page=2,3... 요청 시 AJAX로 partial HTML 반환
+   */
+  @GetMapping("/collectionAjax")
+  @Operation(summary = "한자 목록 AJAX", description = "추가 10개 로딩")
 
-        public String kanjiCollectionAjax(
-                        @AuthenticationPrincipal MyPrincipal principal,
-                        @ModelAttribute KanjiSearchRequest searchRequest,
-                        @RequestParam(name = "page", defaultValue = "1") int page,
-                        Model model) {
+  public String kanjiCollectionAjax(
+      @AuthenticationPrincipal MyPrincipal principal,
+      @ModelAttribute KanjiSearchRequest searchRequest,
+      @RequestParam(name = "page", defaultValue = "1") int page,
+      Model model) {
 
-                // 다음 페이지 로드
-                Page<KanjiSearchResponse> searchResponse = kanjiCollectionService.getMyCollection(principal.getUserId(),
-                                searchRequest, page);
+    // 다음 페이지 로드
+    Page<KanjiSearchResponse> searchResponse = kanjiCollectionService.getMyCollection(principal.getUserId(),
+        searchRequest, page);
 
-                model.addAttribute("searchResponse", searchResponse.getContent());
+    model.addAttribute("searchResponse", searchResponse.getContent());
 
-                // Thymeleaf fragment만 반환
-                return "kanji/kanjiCollectionFragment :: cardList";
-        }
+    // Thymeleaf fragment만 반환
+    return "kanji/kanjiCollectionFragment :: cardList";
+  }
 
-        @GetMapping("/details")
-        @ResponseBody
-        @Operation(summary = "한자 정보", description = "한자의 정보와 수집한 날자를 화면에 전송한다")
-        public KanjiDetailResponse getKanjiDetails(
-                        @RequestParam(name = "kanjiId") Long kanjiId,
-                        @AuthenticationPrincipal MyPrincipal principal) {
-                return kanjiCollectionService.getKanjiDetail(kanjiId, principal.getUserId());
-        }
+  @GetMapping("/details")
+  @ResponseBody
+  @Operation(summary = "한자 정보", description = "한자의 정보와 수집한 날자를 화면에 전송한다")
+  public KanjiDetailResponse getKanjiDetails(
+      @RequestParam(name = "kanjiId") Long kanjiId,
+      @AuthenticationPrincipal MyPrincipal principal) {
+    return kanjiCollectionService.getKanjiDetail(kanjiId, principal.getUserId());
+  }
 
-        @ResponseBody
-        @PostMapping("/addCollection")
-        public void addCollection(
-                        @RequestParam("kanjiId") Long kanjiId,
-                        @AuthenticationPrincipal MyPrincipal principal) {
-                kanjiCollectionService.addCollection(kanjiId, principal.getUserId());
-        }
+  @ResponseBody
+  @PostMapping("/addCollection")
+  public void addCollection(
+      @RequestParam("kanjiId") Long kanjiId,
+      @AuthenticationPrincipal MyPrincipal principal) {
+    kanjiCollectionService.addCollection(kanjiId, principal.getUserId());
+  }
 
-        @GetMapping("/otherCollection")
-        public String otherKanjiCollection(
-                        @RequestParam(name = "userId") Long userId,
-                        @ModelAttribute KanjiSearchRequest searchRequest,
-                        Model model) {
-                int page = 1;
+  @GetMapping("/otherCollection")
+  public String otherKanjiCollection(
+      @RequestParam(name = "userId") Long userId,
+      @ModelAttribute KanjiSearchRequest searchRequest,
+      Model model) {
+    int page = 1;
 
-                Page<KanjiSearchResponse> searchResponse = kanjiCollectionService.getMyCollection(userId,
-                                searchRequest, page);
+    Page<KanjiSearchResponse> searchResponse = kanjiCollectionService.getMyCollection(userId,
+        searchRequest, page);
 
-                model.addAttribute("searchResponse", searchResponse.getContent());
-                ;
+    model.addAttribute("searchResponse", searchResponse.getContent());
+    ;
 
-                return "kanji/otherKanjiCollection";
-        }
+    return "kanji/otherKanjiCollection";
+  }
 
-        /**
-         * 무한 스크롤로 page=2,3... 요청 시 AJAX로 partial HTML 반환
-         */
-        @GetMapping("/otherCollectionAjax")
-        @Operation(summary = "프로필 한자 목록 AJAX", description = "추가 10개 로딩")
-        public String kanjiCollectionAjax(
-                        @RequestParam(name = "userId") Long userId,
-                        @ModelAttribute KanjiSearchRequest searchRequest,
-                        @RequestParam(name = "page", defaultValue = "1") int page,
-                        Model model) {
+  /**
+   * 무한 스크롤로 page=2,3... 요청 시 AJAX로 partial HTML 반환
+   */
+  @GetMapping("/otherCollectionAjax")
+  @Operation(summary = "프로필 한자 목록 AJAX", description = "추가 10개 로딩")
+  public String kanjiCollectionAjax(
+      @RequestParam(name = "userId") Long userId,
+      @ModelAttribute KanjiSearchRequest searchRequest,
+      @RequestParam(name = "page", defaultValue = "1") int page,
+      Model model) {
 
-                // 다음 페이지 로드
-                Page<KanjiSearchResponse> searchResponse = kanjiCollectionService.getMyCollection(userId,
-                                searchRequest, page);
+    // 다음 페이지 로드
+    Page<KanjiSearchResponse> searchResponse = kanjiCollectionService.getMyCollection(userId,
+        searchRequest, page);
 
-                model.addAttribute("searchResponse", searchResponse.getContent());
+    model.addAttribute("searchResponse", searchResponse.getContent());
 
-                // Thymeleaf fragment만 반환
-                return "kanji/kanjiCollectionFragment :: cardList";
-        }
+    // Thymeleaf fragment만 반환
+    return "kanji/kanjiCollectionFragment :: cardList";
+  }
 
-        @ResponseBody
-        @PostMapping("/wrongCountUp")
-        public void wrongCountUp(
-                        @RequestParam("kanjiId") Long kanjiId,
-                        @AuthenticationPrincipal MyPrincipal principal) {
-                kanjiCollectionService.wrongCountUp(kanjiId, principal.getUserId());
-        }
+  @ResponseBody
+  @PostMapping("/wrongCountUp")
+  public void wrongCountUp(
+      @RequestParam("kanjiId") Long kanjiId,
+      @AuthenticationPrincipal MyPrincipal principal) {
+    kanjiCollectionService.wrongCountUp(kanjiId, principal.getUserId());
+  }
 
-        @ResponseBody
-        @PostMapping("/wrongDelete")
-        public void wrongDelete(@RequestParam("kanjiId") Long kanjiId,
-                        @AuthenticationPrincipal MyPrincipal principal) {
-                kanjiCollectionService.wrongDelete(kanjiId, principal.getUserId());
-        }
+  @ResponseBody
+  @PostMapping("/wrongDelete")
+  public void wrongDelete(@RequestParam("kanjiId") Long kanjiId,
+      @AuthenticationPrincipal MyPrincipal principal) {
+    kanjiCollectionService.wrongDelete(kanjiId, principal.getUserId());
+  }
 
-        @ResponseBody
-        @PostMapping("/addBookMark")
-        public void addBookMark(@RequestParam("kanjiId") Long kanjiId,
-                        @AuthenticationPrincipal MyPrincipal principal) {
-                kanjiCollectionService.addBookMark(kanjiId, principal.getUserId());
-        }
+  @ResponseBody
+  @PostMapping("/addBookMark")
+  public void addBookMark(@RequestParam("kanjiId") Long kanjiId,
+      @AuthenticationPrincipal MyPrincipal principal) {
+    kanjiCollectionService.addBookMark(kanjiId, principal.getUserId());
+  }
 
-        @ResponseBody
-        @PostMapping("/deleteBookMark")
-        public void deleteBookMark(@RequestParam("kanjiId") Long kanjiId,
-                        @AuthenticationPrincipal MyPrincipal principal) {
-                kanjiCollectionService.deleteBookMark(kanjiId, principal.getUserId());
-        }
+  @ResponseBody
+  @PostMapping("/deleteBookMark")
+  public void deleteBookMark(@RequestParam("kanjiId") Long kanjiId,
+      @AuthenticationPrincipal MyPrincipal principal) {
+    kanjiCollectionService.deleteBookMark(kanjiId, principal.getUserId());
+  }
 }
