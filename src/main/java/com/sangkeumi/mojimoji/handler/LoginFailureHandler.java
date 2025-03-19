fd346d014d7ded94090c2cfde63837852c98a76e
mk0131 <jmg21128@gmail.com>
2025-03-19 13:27:01 +0900

    Fix: 실행 테스트

--- src/main/java/com/sangkeumi/mojimoji/handler/LoginFailureHandler.java
+++ src/main/java/com/sangkeumi/mojimoji/handler/LoginFailureHandler.java
@@ -16,22 +16,22 @@ import lombok.extern.slf4j.Slf4j;
 @Component
 @Slf4j
 public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
-    @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-            AuthenticationException exception) throws IOException, ServletException {
-        log.info("LoginFailureHandler : 로그인 실패");
-        String errMessage = "";
+  @Override
+  public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException exception) throws IOException, ServletException {
+    log.info("LoginFailureHandler : 로그인 실패");
+    String errMessage = "";
 
-        if (exception instanceof BadCredentialsException) {
-            errMessage = "아이디 또는 비밀번호가 일치하지 않습니다.";
-        } else {
-            errMessage = exception + exception.getMessage();
-            errMessage = "로그인에 실패했습니다. 다시 시도해 주세요";
-        }
-        errMessage = URLEncoder.encode(errMessage, "UTF-8");
-
-        this.setDefaultFailureUrl("/user/login?error=true&message=" + errMessage);
-        super.onAuthenticationFailure(request, response, exception);
+    if (exception instanceof BadCredentialsException) {
+      errMessage = "아이디 또는 비밀번호가 일치하지 않습니다.";
+    } else {
+      errMessage = exception + exception.getMessage();
+      errMessage = "로그인에 실패했습니다. 다시 시도해 주세요";
     }
+    errMessage = URLEncoder.encode(errMessage, "UTF-8");
+
+    this.setDefaultFailureUrl("/user/login?error=true&message=" + errMessage);
+    super.onAuthenticationFailure(request, response, exception);
+  }
 
 }
